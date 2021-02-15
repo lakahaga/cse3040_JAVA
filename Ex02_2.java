2551fc4afa1c5c7f653ea5326b20de2824d4c6af
YERIN CHOI <lakahaga@gmail.com>
2021-02-15 14:08:22 +0900

    Create Ex02_2.java

---
+++ Ex02_2.java
@@ -0,0 +1,17 @@
+package cse3040Ex02;
+
+import java.util.Random;
+
+public class Ex02_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random generator = new Random();
+		System.out.println(generator.nextInt());
+		System.out.println(generator.nextInt());
+		System.out.println(generator.nextInt());
+		System.out.println(generator.nextInt());
+		System.out.println(generator.nextInt());
+	}
+
+}
