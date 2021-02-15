922915fac07ad3dcb85f7458a4b90ffce63a9cc7
YERIN CHOI <lakahaga@gmail.com>
2021-02-15 14:08:20 +0900

    Create Ex02_1.java

---
+++ Ex02_1.java
@@ -0,0 +1,11 @@
+package cse3040Ex02;
+
+public class Ex02_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World");
+		System.out.println("Hello, World".length());
+	}
+
+}
