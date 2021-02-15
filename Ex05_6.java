13adb5cda1c5cc912ad795b39d8ffd8e3f6d6d51
YERIN CHOI <lakahaga@gmail.com>
2021-02-15 14:08:28 +0900

    Create Ex05_6.java

---
+++ Ex05_6.java
@@ -0,0 +1,17 @@
+package ces3040_Ex05;
+
+public class Ex05_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1,sum=0;
+		do
+		{
+			sum+=i;
+			i++;
+		}while(i<=100);
+		System.out.println("sum :"+sum);
+		
+	}
+
+}
