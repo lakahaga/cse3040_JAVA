20863c1e5fd00bc8c6aea6ddf31ec31e98b5d684
YERIN CHOI <lakahaga@gmail.com>
2021-02-15 14:10:37 +0900

    Create Lecture.java

---
+++ Lecture.java
@@ -0,0 +1,46 @@
+package cse3040_Ex07;
+
+//class Employee3 {
+//	String name; //a variable of class Employee 
+//	public void setName(String name)//a method of class Employee 
+//	{
+//		this.name=name; 
+//	}
+//	public String getName()//a method of class Employee 
+//	{
+//		return name;
+//	}
+//}
+//class Employee2{
+//	String name;
+//	public void setName(String name)
+//	{
+//		this.name=name; //색깔 잘 봐!! 다르지!!!!
+		//this -> object itself
+//	}
+//	public char[] getName() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//}
+//public class Lecture {
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Employee3 m = new Employee3();
+//		m.setName("Peter");
+//		System.out.println(m.getName());
+//		//m is instance of class Employee
+//		//We call instance of certain class object except for primitive type 
+//		Employee2 m2= new Employee2();
+//		m2.setName("Peter");
+//		System.out.println(m2.getName());
+//		Employee2 n2= new Employee2();
+//		n2.setName("John");
+//		System.out.println(n2.getName());
+		// name in m2 and name in n2 are independent 
+		
+//	}
+
+//}
+//
