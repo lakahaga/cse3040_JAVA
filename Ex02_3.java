71bd44b7310361948ae77abab92efd8eafdcf7e9
YERIN CHOI <lakahaga@gmail.com>
2021-02-15 14:09:44 +0900

    Create Ex02_3.java

---
+++ Ex02_3.java
@@ -0,0 +1,14 @@
+package cse3040Ex02;
+
+public class Ex02_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// print values from 125 to 134
+		int b= 125;
+		for(int i=1; i<=10; i++)
+			System.out.print(" "+b++);
+		System.out.println();
+	}
+
+}
